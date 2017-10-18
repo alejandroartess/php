a47591264cf86be30f291212cfb70f065b255f89
ALUMNADO\alarso <alarso@hotmail.com>
2017-10-18 11:33:30 +0200

    dos primeras actividades

---
+++ ACT.2.php
@@ -0,0 +1,7 @@
+<?php
+$base="134";
+$altura="89";
+$area=(($base*$altura)/2);
+echo $area
+
+?>
---
+++ ACT.3.php
@@ -0,0 +1,6 @@
+<?php
+$radio="4";
+$pi="3.14159265359";
+$area=(($radio*$radio)*($pi));
+echo $area;
+?>
